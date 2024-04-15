218065ad1132d6507e13ca7ba6ccb7a9c1a68de9
Harsh Mangalam <harshdev8218@gmail.com>
2024-04-15 13:13:33 +0530

    Added daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { CreateTodo } from "./components/create-todo";
+import { Navbar } from "./components/navbar";
+
 function App() {
   return (
     <div>
-      <header>
-        <h1 className="text-center">Todo with Zustand</h1>
-      </header>
+      <Navbar />
+      <main className="py-20 max-w-2xl mx-auto">
+        <CreateTodo />
+      </main>
     </div>
   );
 }
